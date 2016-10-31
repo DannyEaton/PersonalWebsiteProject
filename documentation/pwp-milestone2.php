7a29b5d743523ba75c94bc9302f14f1042aa995a
Daniel Eaton <danieleaton747@gmail.com>
2016-10-31 01:07:04 -0600

    Added Wireframe for Milestone 2

---
+++ documentation/pwp-milestone2.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Daniel Eaton
+ * Date: 10/31/2016
+ * Time: 12:33 AM
+ */
+?>
+
+<html>
+	<body>
+		<p>How I want my index page to look like. Simple navigation bar at the top followed by a simple yet high resolution pictures sliding and covering most of the page. I want it to be kind of a plain image yet something that falls in line with the general idea of the page. Will probably end up being something like a slider like this website: <a href="https://www.netronome.com/">Netronome</a></p>
+		<img src="milestone-2-mockups/home.png" alt="home" />
+
+		<p>If the user clicks on Designs, I want it to take him to this page, it will feature this layout with all of the options in the submenu. If the user were to click on one of these items a modal will appear with this feature (see below) also designed like this however with different designs in that topic. If you you want this all to be a one static page site I can have this load into the page rather than go to a different page, and have the rest of the pages mentioned do the same via ajax html files or angular 2 (if that is possible with Angular since I dont know too much about it yet).</p>
+		<img src="milestone-2-mockups/designs-general-layout-navigation-menus.png" alt="design general layout" />
+		<p>If the user were to click on a feature in designs-general a modal like this would appear displaying the feature to the user with a short sentence at the bottom describing to the user what it does and what its generally used to do.</p>
+		<img src="milestone-2-mockups/designs-modal.png" alt="designs-modal" />
+		<p>Below would be the how I would want my forms to look like on both desktop and mobile browsers. I will want there to be input fields for: "meeting day", "meeting time", "name" (first and last), "company name"(optional), "phone number", and "email". It would all be validated first of course, signifying to the user right away what is wrong with a field if it has been input incorrectly or if it is correct. Will probably slowly transition into a different color (red or green), with message popping up at the top. The site would also email both the user and myself to confirm.</p>
+		<img src="milestone-2-mockups/form.png" alt="form" />
+		<img src="milestone-2-mockups/form-mobile.png" alt="form-mobile" />
+		<p>Would be loaded onto the screen if user clicked on services, would describe who sycamore holler is (company, not myself too give off a more proffessional vibe), what we do, what we hold dear (customer service and other usual talking points a company might say to make itself sound better). Most importantly it would contain statistics and information regarding how purchasing a website will make a customer money and better their image, something that John Staley would find important.</p>
+		<img src="milestone-2-mockups/marketing-about.png" alt="marketing-about" />
+		<p>A page featuring a slider image (an image with each language i use, will probably be some lines of code in a nice 'code-y' font organized yet scattered along the image on a black background).</p>
+		<img src="milestone-2-mockups/technologies.png" alt="technologies" />
+
+		<p>How I intend my index page will look like on mobile using media queries and hamburger menu. I will carry this same general principle along every page of my website, except with the design-general type pages I will split buttons to one per "row". Main menu buttons will be on the bottom, general menu with all features will be at a hamburger menu at the top that expands like a modal. Logo on top will standardly navigate to "home".</p>
+		<img src="milestone-2-mockups/home-mobile.png" alt="home-mobile" />
+
+		<p>In general I feel that none of these pages will be too hard. I am very confident in  my abilities to make this site and I would love approval to make this site a reality. Thanks for taking the time to read through my general strategy of what i plan to do on getting this website done.</p>
+
+	</body>
+</html>
