7604d4df296aeb292b4d65b04435f996f7a85a01
Daniel Eaton <danieleaton747@gmail.com>
2016-11-20 14:13:09 -0700

    added directories and basic files

---
+++ public_html/index.php
@@ -0,0 +1,22 @@
+<!--
+ * Created by PhpStorm.
+ * User: Daniel Eaton
+ * Date: 11/20/2016
+ * Time: 1:32 PM
+ -->
+<html>
+	<head lang="en">
+		<?php
+		//insert meta tags and links to necessary bootstrap/css/jquery files
+		require_once 'util/header.php';
+		?>
+	</head>
+	<body class="black">
+		<nav class="navbar navbar-inverse container-fluid">
+			<div class="navbar-header">
+				<a class="navbar-brand" href="#">Sycamore Holler</a>
+			</div>
+		</nav>
+	</body>
+</html>
+
